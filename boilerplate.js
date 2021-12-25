3cb4e94f388542136c98c19168cb91c5d3c26d15
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2021-12-25 11:26:45 +0530

    made a class instead of functions

--- boilerplate.js
+++ boilerplate.js
@@ -56,73 +56,90 @@ if (!file) {
 
 // const readFile = fs.readFileSync(file, "utf-8").toString().split(/[\r\n]+/);
 
-async function processLines() {
-    const fileStream = fs.createReadStream(file);
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-    });
-    let index = 0;
-    let currFile;
-    for await (const line of rl) {
-        console.log(await currFile, line)
+// async function processLines() {
+//     const fileStream = fs.createReadStream(file);
+//     const rl = readline.createInterface({
+//         input: fileStream,
+//         crlfDelay: Infinity
+//     });
+//     let index = 0;
+//     let currFile;
+//     for await (const line of rl) {
+//         console.log(await currFile, line)
 
-        if (index === 0) {
-            if (boilerplateType !== line.split("# ")[1]) {
-                throw new Error("The boilerplate type does not match the file");
-            }
-        } else {
-            currFile = await processLine(line, outputDir, currFile) || currFile;
-        }
-        index++;
-    }
-}
-
-processLines();
-
-async function processLine(line, outputDir, currFile) {
-    try {
-        await fs.promises.mkdir(outputDir, { recursive: true });
-        if (line.startsWith("# ")) {
-            const filename = line.split("# ")[1];
-            const filepath = path.resolve(outputDir, filename);
-            console.log("Creating file: " + filepath);
-            await writeFile(filepath, "");
-            return filepath;
-        } else {
-            // console.log(await currFile, line)
-            await fs.promises.appendFile(await currFile, line + "\n");
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
-// if (boilerplateType === readFile[0].split("# ")[1]) {
-
-//     createFilesAndWrite(outputDir, readFile);
-
-// } else {
-//     console.log("Wrong boilerplate type");
-// }
-
-// async function createFilesAndWrite(outputPath, readFile) {
-//     try {
-//         await fs.promises.mkdir(outputPath, { recursive: true });
-//         let currFile;
-//         for (let i = 1; i < readFile.length; i++) {
-//             if (readFile[i].startsWith("# ")) {
-//                 const filename = readFile[i].split("# ")[1];
-//                 const filepath = path.resolve(outputPath, filename);
-//                 console.log("Creating file: " + filepath);
-//                 await writeFile(filepath, "");
-//                 currFile = filepath;
-//             } else {
-//                 await fs.promises.appendFile(currFile, readFile[i] + "\n",);
+//         if (index === 0) {
+//             if (boilerplateType !== line.split("# ")[1]) {
+//                 throw new Error("The boilerplate type does not match the file");
 //             }
+//         } else {
+//             currFile = await processLine(line, outputDir, currFile) || currFile;
 //         }
-//     } catch (error) {
-//         throw new Error(error);
+//         index++;
 //     }
 // }
+
+
+// async function processLine(line, outputDir, currFile) {
+//     try {
+//         await fs.promises.mkdir(outputDir, { recursive: true });
+//         if (line.startsWith("# ")) {
+//             const filename = line.split("# ")[1];
+//             const filepath = path.resolve(outputDir, filename);
+//             console.log("Creating file: " + filepath);
+//             await writeFile(filepath, "");
+//             return filepath;
+//         } else {
+//             // console.log(await currFile, line)
+//             await fs.promises.appendFile(await currFile, line + "\n");
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+class Boilerplate {
+    constructor(file, output, type) {
+        this.output = output;
+        this.type = type;
+        this.currFile;
+        this.fileStream = fs.createReadStream(file);
+    }
+
+    async processLines() {
+        const rl = readline.createInterface({
+            input: this.fileStream,
+            crlfDelay: Infinity
+        });
+        let index = 0;
+        for await (const line of rl) {
+            console.log(this.currFile, line)
+
+            if (index === 0) {
+                if (boilerplateType !== line.split("# ")[1])
+                    throw new Error("The boilerplate type does not match the file");
+            } else {
+                this.currFile = await this.processLine(line) || this.currFile;
+            }
+            index++;
+        }
+    }
+
+    async processLine(line) {
+        try {
+            await fs.promises.mkdir(this.output, { recursive: true });
+            if (line.startsWith("# ")) {
+                const filename = line.split("# ")[1];
+                this.currFile = path.resolve(this.output, filename);
+                console.log("Creating file: " + this.currFile);
+                await writeFile(this.currFile, "");
+            } else {
+                // console.log(await currFile, line)
+                await fs.promises.appendFile(this.currFile, line + "\n");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+new Boilerplate(file, outputDir, boilerplateType).processLines();
