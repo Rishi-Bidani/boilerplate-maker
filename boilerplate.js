cc6ba781ca2be794e9d391f785e06695a778033c
Rishi <64310471+Rishi-Bidani@users.noreply.github.com>
2021-12-25 12:39:15 +0530

    resolved certain errors, caused by transition to classes

--- boilerplate.js
+++ boilerplate.js
@@ -39,63 +39,6 @@ function getArgs() {
     }
     return args;
 }
-const args = getArgs();
-
-const file = args["-f"];
-const boilerplateType = args["-t"];
-const outputDir = args["-o"];
-
-// Check if argument is supplied
-if (!file) {
-    throw new Error("Please provide a file");
-} else if (!boilerplateType) {
-    throw new Error("Please provide a boilerplate type");
-} else if (!outputDir) {
-    throw new Error("Please provide an output directory");
-}
-
-// const readFile = fs.readFileSync(file, "utf-8").toString().split(/[\r\n]+/);
-
-// async function processLines() {
-//     const fileStream = fs.createReadStream(file);
-//     const rl = readline.createInterface({
-//         input: fileStream,
-//         crlfDelay: Infinity
-//     });
-//     let index = 0;
-//     let currFile;
-//     for await (const line of rl) {
-//         console.log(await currFile, line)
-
-//         if (index === 0) {
-//             if (boilerplateType !== line.split("# ")[1]) {
-//                 throw new Error("The boilerplate type does not match the file");
-//             }
-//         } else {
-//             currFile = await processLine(line, outputDir, currFile) || currFile;
-//         }
-//         index++;
-//     }
-// }
-
-
-// async function processLine(line, outputDir, currFile) {
-//     try {
-//         await fs.promises.mkdir(outputDir, { recursive: true });
-//         if (line.startsWith("# ")) {
-//             const filename = line.split("# ")[1];
-//             const filepath = path.resolve(outputDir, filename);
-//             console.log("Creating file: " + filepath);
-//             await writeFile(filepath, "");
-//             return filepath;
-//         } else {
-//             // console.log(await currFile, line)
-//             await fs.promises.appendFile(await currFile, line + "\n");
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
 
 class Boilerplate {
     constructor(file, output, type) {
@@ -112,13 +55,11 @@ class Boilerplate {
         });
         let index = 0;
         for await (const line of rl) {
-            console.log(this.currFile, line)
-
             if (index === 0) {
                 if (boilerplateType !== line.split("# ")[1])
                     throw new Error("The boilerplate type does not match the file");
             } else {
-                this.currFile = await this.processLine(line) || this.currFile;
+                await this.processLine(line);
             }
             index++;
         }
@@ -133,13 +74,29 @@ class Boilerplate {
                 console.log("Creating file: " + this.currFile);
                 await writeFile(this.currFile, "");
             } else {
-                // console.log(await currFile, line)
-                await fs.promises.appendFile(this.currFile, line + "\n");
+                console.log(this.currFile, line)
+                if (typeof this.currFile === "string") await fs.promises.appendFile(this.currFile, line + "\n");
             }
         } catch (error) {
-            console.log(error);
+            console.log("Error: " + error);
         }
     }
+}
+
+
+const args = getArgs();
+
+const file = args["-f"];
+const boilerplateType = args["-t"];
+const outputDir = args["-o"];
+
+// Check if argument is supplied
+if (!file) {
+    throw new Error("Please provide a file");
+} else if (!boilerplateType) {
+    throw new Error("Please provide a boilerplate type");
+} else if (!outputDir) {
+    throw new Error("Please provide an output directory");
 }
 
 new Boilerplate(file, outputDir, boilerplateType).processLines();
